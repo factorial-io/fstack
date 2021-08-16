7d6a2b2ec2060e8df2fa60083c469bd8d834145b
Michael Großklaus <michael@factorial.io>
2021-08-16 16:48:40 +0200

    feat(html): create .htmlvalidate.js when adding package via "factorial init"

--- packages/html/index.js
+++ packages/html/index.js
@@ -5,6 +5,15 @@ const lint = require("./lib/lint");
 const htmlValidateConfig = require(path.join(__dirname, ".htmlvalidate.json")); // eslint-disable-line
 
 module.exports = {
+  configFiles: [
+    {
+      name: ".htmlvalidate.js",
+      content: `const htmlValidateConfig = require("@factorial/stack-html").htmlValidate;
+
+module.exports = htmlValidateConfig;
+`,
+    },
+  ],
   htmlValidate: htmlValidateConfig,
   type: "html",
   extensions: ["html"],
