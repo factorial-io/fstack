2e2fdbffb9585c871eaccb40efe21812fee0304c
Michael Großklaus <michael@factorial.io>
2022-02-03 13:53:44 -0800

    fix(vue): use only eslint-plugin-vue to fix prettier integration

--- packages/stack-vue/.eslintrc.js
+++ packages/stack-vue/.eslintrc.js
@@ -1,16 +1,12 @@
-const eslintConfig = require("@factorial/stack-javascript").eslint;
-const deepMerge = require("deepmerge");
+const eslintConfig = require("@factorial/stack-javascript").eslint
+const deepMerge = require("deepmerge")
 
 module.exports = deepMerge(eslintConfig, {
-  extends: ["plugin:vue/recommended", "plugin:prettier-vue/recommended"],
-  plugins: ["vue"],
-  rules: {
-    "generator-star-spacing": "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "vue/no-v-html": "off",
-    "prettier-vue/prettier": "error",
-  },
+  extends: ["plugin:vue/essential"],
   globals: {
     $nuxt: true,
   },
-});
+  env: {
+    node: true,
+  },
+})
