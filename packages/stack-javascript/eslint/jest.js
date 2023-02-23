dd88d013ec7e47cd15fd430e2cc8ee83a57c0654
Fynn Becker <post@fynn.be>
2023-02-23 10:45:52 +0100

    feat(javascript): Use Jest glob pattern for ESLint config

--- packages/stack-javascript/eslint/jest.js
+++ packages/stack-javascript/eslint/jest.js
@@ -1,7 +1,7 @@
 module.exports = {
   overrides: [
     {
-      files: ["**/__tests__/**/*.spec.js"],
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
       extends: ["plugin:jest/recommended"],
       rules: {
         "global-require": 0,
